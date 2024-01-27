b0d98a205acab9db2f9310e911bda9743b71b23b
jcanales <jcanales@webdoxclm.com>
2024-01-27 00:31:58 -0300

    feat: created plug for authorization and use current user plug

--- lib/ironiauth_web/controllers/user_controller.ex
+++ lib/ironiauth_web/controllers/user_controller.ex
@@ -5,6 +5,26 @@ defmodule IroniauthWeb.UserController do
   alias Ironiauth.Accounts.User
 
   action_fallback IroniauthWeb.FallbackController
+  plug :is_authorized_user when action in [:update, :delete]
+
+  defp is_authorized_user(conn, _params) do
+    user_id = conn.params["id"]
+    with {:ok, user} <- Accounts.get_user_by_uuid(user_id, true),
+        session_user <- conn.assigns.current_user do
+        if user.uuid == session_user.uuid do
+          conn
+        else
+          conn
+          |> json(%{error: "Operation not permitted"})
+          |> halt()
+        end
+    else
+      {:error, msg} ->
+        conn
+        |> json(%{error: "resource not permitted"})
+        |> halt()
+    end
+  end
 
   def index(conn, _params) do
     users = Accounts.list_users()
@@ -17,14 +37,11 @@ defmodule IroniauthWeb.UserController do
   end
 
   def me(conn, _params) do
-    user = Guardian.Plug.current_resource(conn)
-    conn |> render("user.json", user: user)
+    conn |> render("user.json", user: conn.assigns.current_user)
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Accounts.get_user!(id)
-
-    with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
+    with {:ok, %User{} = user} <- Accounts.update_user(conn.assigns.current_user, user_params) do
       render(conn, :show, user: user)
     end
   end
