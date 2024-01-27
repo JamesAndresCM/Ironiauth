1c3e482960e862594c87368b0a66f0d41c73c69e
jcanales <jcanales@webdoxclm.com>
2024-01-27 20:49:11 -0300

    chore: create seeds for company and roles

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,32 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Ironiauth.Accounts.Role
+alias Ironiauth.Management.Company
+alias Ironiauth.Repo
+
+Enum.map(Ironiauth.Accounts.Role.role_types, fn {x, _} -> Repo.insert(%Role{name: x}) end)
+
+companies = [
+  %{
+    name: "coca cola",
+    domain: "cocacolacompany.com",
+    inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now, :second),
+    updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now, :second)
+  },
+  %{
+    name: "nike",
+    domain: "nike.com",
+    inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now, :second),
+    updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now, :second)
+  },
+  %{
+    name: "snk",
+    domain: "snk.com",
+    inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now, :second),
+    updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now, :second)
+  }
+]
+
+Repo.insert_all(Company, companies)
