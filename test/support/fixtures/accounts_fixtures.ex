7daafe7a0d4bbe178b1a336bc7743212b2ba13d7
jcanales <jcanales@webdoxclm.com>
2024-01-27 20:49:48 -0300

    chore: modify fixtures

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -4,10 +4,13 @@ defmodule Ironiauth.AccountsFixtures do
   entities via the `Ironiauth.Accounts` context.
   """
 
+  alias Ironiauth.Accounts.Role
+  alias Ironiauth.Repo
   @doc """
   Generate a unique user email.
   """
-  def unique_user_email, do: "some email#{System.unique_integer([:positive])}"
+  @counter :rand.uniform()
+  def unique_user_email, do: "some_email#{@counter}" <> "@example.com"
 
   @doc """
   Generate a unique user username.
@@ -22,11 +25,21 @@ defmodule Ironiauth.AccountsFixtures do
       attrs
       |> Enum.into(%{
         email: unique_user_email(),
-        password_hash: "some password_hash",
-        username: unique_user_username()
+        password: "password_hash",
+        password_confirmation: "password_hash",
+        username: unique_user_username(),
       })
       |> Ironiauth.Accounts.create_user()
 
     user
+  end
+
+  def role_fixture(attrs \\ %{}) do
+    {:ok, role} = Repo.insert(%Role{name: attrs[:name]})
+    role
+  end
+
+  def create_default_roles() do
+    Enum.map(Ironiauth.Accounts.Role.role_types, fn {x, _} -> Repo.insert(%Role{name: x}) end)
   end
 end
